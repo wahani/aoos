27729a19a864cf5827184e24871a34412df14711
Sebastian <wahani@gmail.com>
2014-11-09 13:09:32 +0100

    summary method for oom

--- R/oom.R
+++ R/oom.R
@@ -1,20 +1,26 @@
 #' Class oom
 #' 
-#' This is an environment with some methods for access restriction. Don't use it.
+#' This is an environment with some methods. Every class defined by \code{defineClass} will inherit from oom.
 #' 
 #' @import methods
 #' @exportClass oom
 #' @rdname oom
-setClass("oom", contains = "environment")
+setClass("oom", contains = c("environment", "VIRTUAL"))
 
+#' @rdname oom
+#' @export
 setMethod("show", signature = c(object = "oom"), 
           function(object) {
+            cat("Approximated memory size:", 
+                sum(envSize(parent.env(object))$Size), 
+                "(Mib)\n")
             cat("public member:\n")
             lapply(ls(object), function(n) cat(" ", n, "\n"))
             #             print(env.profile(as.environment(object)))
           })
 
 #' @rdname oom
+#' @export
 #' @param x object
 #' @param name member name
 setMethod("$", signature = c(x = "oom"),
@@ -22,13 +28,46 @@ setMethod("$", signature = c(x = "oom"),
             if(exists(name, envir = x, inherits = FALSE)) {
               get(name, envir = x)
             } else {
-              stop(paste(name, "is not public."))
+              stop(paste(name, "is not a public member."))
             }
           })
 
 #' @rdname oom
+#' @export
 #' @param value value to assign to. Will throw an error.
 setMethod("$<-", signature = c(x = "oom"),
           function(x, name, value) {
             stop("If you want to add public fields use 'publicValue' in the class definition.")
           })
+
+#' @rdname oom
+#' @param object object
+#' @param ... arguments passed to method (not used).
+#' @export
+setMethod("summary", signature = c(object = "oom"),
+          function(object, ...) {
+            envSize(parent.env(object))
+          })
+
+envSize <- function (env) {
+  
+  napply <- function(names, fn) sapply(names, function(x) fn(get(x, pos = env)))
+  
+  names <- ls(env, all.names = TRUE)
+  
+  obj.class <- napply(names, function(x) as.character(class(x))[1])
+  obj.mode <- napply(names, mode)
+  obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
+  obj.size <- napply(names, object.size)
+  
+  obj.subsizes <- napply(names, function(x) {
+    if(inherits(x, "publicFunction") && !identical(environment(x), env)) 
+      envSize(environment(x))
+  })
+  
+  obj.subsizes <- do.call(rbind, obj.subsizes)
+  
+  out <- rbind(data.frame(Type = obj.type, "Size.Mib" = round(obj.size / (1024^2), 1)), obj.subsizes)
+  out[order(rownames(out), out$Type), ]
+  
+}
---
+++ tests/testthat/test-summary.R
@@ -0,0 +1,19 @@
+context("Summary")
+test_that("Summary", {
+  suppressWarnings({
+    test <- defineClass("test", {
+      x <- publicValue(1)
+      y <- NULL
+      doSomething <- publicFunction(function() {
+        y <<- y + 1
+        invisible(self)
+      })
+    })
+  })
+    
+  instance <- test()
+  df <- summary(instance)
+  expect_is(df, "data.frame")
+  expect_equal(nrow(df), 6)
+  expect_equal(rownames(df), c("doSomething", "self", "x", "x.validity", "x.x", "y"))
+})
