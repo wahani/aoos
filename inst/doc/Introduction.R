fb78b596ead06c3ca4f0571b5fab631a5a092d2f
Sebastian <wahani@gmail.com>
2015-04-12 10:07:31 +0200

    update vignettes

--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -1,54 +1,6 @@
 ## ----, eval=FALSE--------------------------------------------------------
 #  vignette("aoosClasses", "aoos")
 
-## ----, echo=FALSE--------------------------------------------------------
-removeClass("PersonRC")
-
-## ------------------------------------------------------------------------
-library(aoos)
-
-PersonRC <- Class({
-  Class <- "PersonRC"
-  
-  personName <- "character"
-  
-  initialize <- function(name) {
-    .self$personName <- name
-    .self$greet()
-    }
-  
-  greet <- function() {
-    cat(paste0("Hello, my name is ", .self$personName, ".\n"))
-    }
-  
-  })
-
-ann <- PersonRC("Ann")
-ann
-ann$personName
-ann$personName <- "not Ann"
-ann$greet()
-
-## ------------------------------------------------------------------------
-PrivatePerson <- Class({
-  Class <- "PrivatePerson"
-  contains <- "Private"
-  
-  .personName <- "character"
-  
-  initialize <- function(name) {
-    .self$.personName <- name
-    .self$greet()
-    }
-  
-  greet <- function() {
-    cat(paste0("Hello, my name is ", .self$.personName, ".\n"))
-    }
-  
-  })
-
-ann <- PrivatePerson("Ann")
-ann
-stopifnot(inherits(try(ann$.personName, silent = TRUE), "try-error"))
-ann$greet()
+## ----, eval=FALSE--------------------------------------------------------
+#  vignette("referenceClasses", "aoos")
 
