041dde4cd74b6692787d0d0d88cf08d66f355643
Sebastian <wahani@gmail.com>
2015-06-29 15:21:10 +0200

    Infix functions new select next method with NextMethod

--- tests/testthat/test-Infix.R
+++ tests/testthat/test-Infix.R
@@ -23,3 +23,17 @@ test_that("Infix operators for S3", {
   expect_false(Test(2) >= 3)
   
 })
+
+test_that("Infix selects next method.", {
+  Test <- function(.x) {
+    ".+" <- function(e2) Test(getX() + e2)
+    ".==" <- function(e2) getX() == e2
+    getX <- function() .x
+    retList(c("Test", "Infix"))
+  }
+  
+  expect_true(Test(2) + 2 == 4)
+  expect_error(Test(2) - 2 == 4)
+  
+})
+
